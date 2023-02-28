144fd6db13580415bd48a2cde5b987a379f50e40
Bohdan <93320587+Gembara@users.noreply.github.com>
2023-02-28 12:54:03 +0200

    hm1

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* double r, length, area, volume;
+             double PI = 3.14;
+             Console.WriteLine("виберіть радіус кола\t");        \\ 3 завдання
+             r = double.Parse(Console.ReadLine());
+             length = 2 * PI * r;
+             Console.WriteLine("довжина :  " + length);
+             volume = 4 / 3 * PI * r * r * r; ;
+             Console.WriteLine("обсяг:   " + volume);
+
+             area = PI* r * r;
+             Console.WriteLine("область:    " + area);
+             Console.ReadLine();*/
+
+
+
+           /* string name;
+            int age;
+
+            Console.WriteLine("What is your name?");
+
+            name= Console.ReadLine();
+
+            Console.WriteLine("How old are you " + name + " ?");  \\ 2 завдання
+            
+            age = int.Parse(Console.ReadLine());
+
+            Console.ReadLine(); */
+
+
+          
+
+
+            
+         
+           
+            
+
+
+        }
+    }
+}
